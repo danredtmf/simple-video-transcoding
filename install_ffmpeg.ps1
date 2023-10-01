d04003ea57a5e813df64cb36a6c012e226af48ef
Daniil <57467826+danredtmf@users.noreply.github.com>
2023-10-01 18:16:17 +0700

    Add Install FFmpeg Script
    - для сокращения размера репозитория

---
+++ install_ffmpeg.ps1
@@ -0,0 +1,36 @@
+$current_path = Get-Location
+$resources_path = Join-Path -Path $current_path -ChildPath 'resources'
+$ffmpeg_zip_path = Join-Path -Path $resources_path -ChildPath 'ffmpeg.zip'
+$ffmpeg_folder_path = Join-Path -Path $resources_path -ChildPath 'ffmpeg'
+$ffmpeg_bin_path = Join-Path -Path $ffmpeg_folder_path -ChildPath 'bin'
+$ffmpeg_path = Join-Path -Path $ffmpeg_bin_path -ChildPath 'ffmpeg.exe'
+
+function Check-Resources {
+    if (Test-Path -Path $resources_path) {
+        Write-Host 'Скачивание FFmpeg' -f Green
+        Download-FFmpeg
+    }
+    else {
+        Write-Host 'Создание папки "resources"...' -f Green
+        New-Item -Path $resources_path -ItemType Directory | Out-Null
+        Check-Resources
+    }
+}
+
+
+function Download-FFmpeg {
+    wget https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip -outfile $ffmpeg_zip_path
+    Get-FFmpeg
+
+}
+
+function Get-FFmpeg {
+    Expand-Archive -Path $ffmpeg_zip_path -DestinationPath $resources_path
+    Rename-Item -Path (Get-ChildItem -Path $resources_path -Filter "ffmpeg-*").FullName -NewName "ffmpeg"
+    Remove-Item -Path $ffmpeg_zip_path
+    Move-Item -Path $ffmpeg_path -Destination $resources_path
+    Remove-Item -Path $ffmpeg_folder_path -Recurse
+
+}
+
+Check-Resources

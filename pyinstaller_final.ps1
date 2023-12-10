dca93f7641619b0d4fb08f92bdf30745660ad9bc
Daniil <57467826+danredtmf@users.noreply.github.com>
2023-12-10 23:50:17 +0700

    Update pyinstaller_final.ps1
    В способе сборке программы через pyinstaller добавлены бинарные файлы. Теперь данная сборка будет работать, но по производительности будет уступать способу через Nuitka

--- pyinstaller_final.ps1
+++ pyinstaller_final.ps1
@@ -1,2 +1,5 @@
 ./venv/Scripts/activate.ps1;`
-pyinstaller --windowed --onefile --name "Simple Video Transcoding" main.py;
+pyinstaller --windowed --onefile `
+--add-binary "./resources/ffmpeg.exe;."`
+--add-binary "./venv/Lib/site-packages/pymediainfo/MediaInfo.dll;."`
+--name "Simple Video Transcoding" main.py;

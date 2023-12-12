2ef7efb14f9fb595c0cfcd88a7f57fdc7cdee112
Daniil <57467826+danredtmf@users.noreply.github.com>
2023-12-12 23:15:22 +0700

    Update global_vars.py
    Удалил неиспользуемые переменные

--- global_vars.py
+++ global_vars.py
@@ -4,9 +4,6 @@ ffmpeg_presets: tuple = ('Simple Encoding - Decoding', 'Simple Cut')
 video_formats: tuple = ('.ogv', '.mp4 (H264)', '.mp4 (H265)')
 video_file_types: tuple = (('Video Files', ('*.mp4', '*.ogv', '*.mkv')),)
 
-win_main_min_size: tuple = ()
-win_main_min_size_encdec_simple: tuple = ()
-
 is_debug: bool = False
 ffmpeg_path = "./resources/ffmpeg.exe" if is_debug else os.path.join(os.path.dirname(__file__), "ffmpeg.exe")
 
